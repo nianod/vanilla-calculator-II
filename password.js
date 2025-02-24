b69b908faff9523f48c5add7f7bb0cb5f5ddfe79
nianod <arnoldkk422@gmail.com>
2025-02-24 22:37:59 +0300

    my second commit II

--- password.js
+++ password.js
@@ -1,20 +1,72 @@
 
-const readline = require("readline")
-const rl = readline.createInterface ({
-   input:process.stdin,
-   output:process,stdout
-});
-rl.question("Enter your password: ", function(password){
-    if(password.includes(",") || password.includes("?") || password.includes("!") || password.includes("@") ) {
-        console.log("Strong Password");
+// const { log } = require("console");
+// const readline = require("readline")
+// const rl = readline.createInterface ({
+//    input:process.stdin,
+//    output:process.stdout
+// });
+// rl.question("Enter your password: ", function(password){
+//     if(password.includes(",") || password.includes("?") || password.includes("!") || password.includes("@") ) {
+//         console.log("Strong Password");
         
-    } else if (password.includes("")) {
-        console.log("your password is very weak");
+//     } else if (password.length < 6) {
+//         console.log("your password is very weak");
         
+//     }
+//     else {
+//         console.log("your password is not strong");
+        
+//     }
+//     rl.close();
+// });
+
+
+
+const prompt = require("prompt-sync")();
+
+const myObjects = {
+    strongest: ["!", "@", "#", "$", "?"],
+    stronger: [",", ":"]
+};
+
+const checkers = ".,!?;:\"'-_()[]{}\\/|&*@#%$^~`";
+const uppercaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const password = () => {
+    while (true) {
+        const getPassword = prompt("Enter your password(Type exit to escape): ");
+
+        // Check if the password contains at least one symbol
+        const hasSymbol = checkers.split("").some(char => getPassword.includes(char));
+
+        // Check if the ``````````````````````````````````````````````````password contains at least one uppercase letter
+        const hasUppercase = uppercaseString.split("").some(char => getPassword.includes(char));
+
+        
+            if(hasSymbol){
+                console.log("Partially weak password");
+            } else{
+                console.log("Partially weak password")
+            }
+        
+        else {
+            if (["S", "E", "V", "X"].some(char => getPassword.includes(char))) {
+                console.log("Strong password");
+            } 
+            else if (getPassword.length <= 2) {
+                console.log("Enter a valid password");
+            } 
+            else {
+                console.log("Your password is weak");
+            }
+            if(getPassword.toLocaleLowerCase() === "exit"){
+                console.log("Exiting...")
+                break;
+            }
+        }
+
     }
-    else {
-        console.log("your password is not strong");
-        
-    }
-    rl.close();
-});
+    
+};
+
+password();
